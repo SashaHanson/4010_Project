31a2bdada42c050c6757f8c9b7cbfa9ef5c84b66
leowallner <lwallner@uwo.ca>
2025-12-01 12:09:49 -0500

    ading more comments

--- temp_delete.py
+++ temp_delete.py
@@ -1,6 +1,9 @@
 ﻿import os, shutil
 from modal import Volume
 
+
+#modal does not allow users to delete stuff directly from the volume, so we need to run a script
+#to be able to delete this stuff
 v = Volume.from_name("dataset")
 v.mount("/data")
 
